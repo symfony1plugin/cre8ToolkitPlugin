aaaa2738bc1753aa6f884a0734d1f8a0a1709632
TehCrow <TehCrow@3c5571db-af2c-42a9-8d5c-01322bf8fdae>
2009-10-23 11:11:04 +0000

    structure changed
    git-svn-id: http://213.171.196.75/svn/cre8/projects/plugins/symfony/cre8ToolkitPlugin/trunk@957 3c5571db-af2c-42a9-8d5c-01322bf8fdae

---
+++ config/cre8ToolkitPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * cre8ToolkitPlugin configuration.
+ * 
+ * @package     cre8ToolkitPlugin
+ * @subpackage  config
+ * @author      The Partners: Bogumil, Mirek
+ * @version     SVN: $Id: PluginConfiguration.class.php 12675 2008-11-06 08:07:42Z Kris.Wallsmith $
+ */
+class cre8ToolkitPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
